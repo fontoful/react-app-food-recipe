e84bf03322b3023b044907e1a7411267da45ca3e
hectoralfonso <hectoralfonso@penguin>
2019-11-08 21:53:59 -0600

    router configured

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import App from "../App";
+
 const Router = () => {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={App} exact />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
