52c373dbcb6381d5c5e792b085a3dbec35d101f5
hectoralfonso <hectoralfonso@penguin>
2019-11-08 21:39:06 -0600

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Navbar from "./styles/Navbar";
 
---
+++ src/components/Router.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router";
+
+const Router = () => {
+	return <div></div>;
+};
+
+export default Router;
