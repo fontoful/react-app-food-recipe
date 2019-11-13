19f48bbc7db1e7d1d9bba40b0ae11b27dc383a02
fontoful <40501207+fontoful@users.noreply.github.com>
2019-11-13 00:57:27 -0600

    added box-shadow & translate y on hover

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import "./Recipes.scss";
 
 const Recipes = ({ recipes }) => {
-	return (
-		<div className="cards">
-			{recipes.map(item => {
-				return (
-					<div key={item.id}>
-						<div className="card">
-							<img src={item.image_url} alt="" />
-							<h2>{item.title}</h2>
-							<p>Publisher : {item.publisher}</p>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+  console.log(recipes);
+
+  return (
+    <div className="cards">
+      {recipes.map(item => {
+        return (
+          <div key={item.id}>
+            <div className="card">
+              <img src={item.image_url} alt="" />
+              <h2>{item.title}</h2>
+              <p>Publisher : {item.publisher}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Recipes;
