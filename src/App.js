ed7a7e5f39c67969a700276b1cadd8bc40017f11
Hector Serrano <fonto.serrano@gmail.com>
2019-11-27 10:32:24 -0600

    added new API documentation

--- src/App.js
+++ src/App.js
@@ -6,33 +6,42 @@ import Recipes from "./components/recipes/Recipes";
 
 const API_KEY = "43297c46c87149f3a14cc5e5c316ea83";
 
+/* 
+New API key = 2f2421a275edcfab73629ca421dfbb5e	
+
+APP ID = b4dee892
+
+DOCUMENTATION : https://developer.edamam.com/edamam-docs-recipe-api
+
+*/
+
 function App() {
-    const [recipe, setRecipe] = useState([]);
+	const [recipe, setRecipe] = useState([]);
 
-    const getRecipe = async e => {
-        // prevent default
-        e.preventDefault();
+	const getRecipe = async e => {
+		// prevent default
+		e.preventDefault();
 
-        // get input
-        const name = e.target.elements.recipeName.value;
+		// get input
+		const name = e.target.elements.recipeName.value;
 
-        const response = await fetch(
-            `https://www.food2fork.com/api/search?key=${API_KEY}&q=${name}&count=15`
-        );
-        const data = await response.json();
-        const recipes = data.recipes;
+		const response = await fetch(
+			`https://www.food2fork.com/api/search?key=${API_KEY}&q=${name}&count=15`
+		);
+		const data = await response.json();
+		const recipes = data.recipes;
 
-        // Setter
-        setRecipe(recipes);
-    };
+		// Setter
+		setRecipe(recipes);
+	};
 
-    return (
-        <>
-            <Navbar />
-            <Form handleSubmit={getRecipe} />
-            <Recipes recipes={recipe} />
-        </>
-    );
+	return (
+		<>
+			<Navbar />
+			<Form handleSubmit={getRecipe} />
+			<Recipes recipes={recipe} />
+		</>
+	);
 }
 
 export default App;
